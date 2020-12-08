824f022cc631d712768d14eca8be0c9f881bdc33
LIJIANJUN <matwetu@gmail.com>
2020-12-08 16:04:02 +0800

    fixed init order

--- linux/bt.cpp
+++ linux/bt.cpp
@@ -7,8 +7,49 @@
 #include <stdio.h>
 
 // assume that dso's will never be unloaded
-static std::map<void*, std::string> ptr2sym;
-static std::shared_mutex lock;
+
+typedef std::map<void*, std::string> TyPtr2Sym;
+struct SymCache{
+
+  
+  void lock(){
+    lck.lock();
+  }
+  
+  void unlock(){
+    lck.unlock();
+  }
+
+  void lock_shared(){
+    lck.lock_shared();
+  }
+
+  void unlock_shared(){
+    lck.unlock_shared();
+  }
+
+
+  const char* get(void* addr) const{
+    auto it = store.find(addr);
+    if (it == store.end()) return nullptr;
+    return it->second.c_str();
+  }
+  
+  const char* set(void* addr, const char* s){
+    auto& ret = (store[addr] = s);
+    return ret.c_str();
+  }
+
+  std::map<void*, std::string> store;
+  std::shared_mutex lck;
+};
+
+SymCache* get_ptr2sym(){
+  static SymCache* _ptr2sym = NULL;
+  if (_ptr2sym) return _ptr2sym;
+  _ptr2sym = new SymCache();
+  return _ptr2sym;
+}
 
 extern "C" char** my_backtrace_symbols(void** ptrs, int count){
   void* my_ptrs[256];
@@ -16,27 +57,32 @@ extern "C" char** my_backtrace_symbols(void** ptrs, int count){
   char** my_ret = NULL;
   char** ret = (char**)malloc(count * sizeof(char*));
   memset(ret, 0, sizeof(char*) * count);
-
-  lock.lock_shared();
+  SymCache* sc = get_ptr2sym();
+  sc->lock_shared();
   for(int i = 0; i < count; ++i){
-    auto it = ptr2sym.find(ptrs[i]);
-    if (it == ptr2sym.end()){
-      my_ptrs[my_count++] = ptrs[i];
+    auto v = sc->get(ptrs[i]);
+    if (v){
+      ret[i] = (char*) v;
     } else {
-      ret[i] = (char*)it->second.c_str();
+      my_ptrs[my_count++] = ptrs[i];
     }
   }
-  lock.unlock_shared();
+  sc->unlock_shared();
   if (my_count){
     my_ret = backtrace_symbols(my_ptrs, my_count);
-    lock.lock();
+    sc->lock();
     for(int j = 0, i = 0; j < count && i < my_count; j++){
       if (ret[j] == NULL){
-        auto& inserted = (ptr2sym[ptrs[j]] = my_ret[i++]);
-        ret[j] = (char*) inserted.c_str();
+        auto v = sc->get(ptrs[j]);
+        if (v){
+          ret[j] = (char*) v;
+        } else {
+          auto vv = sc->set(ptrs[j], my_ret[i++]);
+          ret[j] = (char*) vv;
+        }
       }
     }
-    lock.unlock();
+    sc->unlock();
   }
   free(my_ret);
   return ret;
